6afdf6d0b272d0e190e3acd2a9efeaed6c08e838
Bjorn <rster2002dev@gmail.com>
2023-07-18 21:40:58 +0200

    🧪 Create tests for guarding aud en iss claims

--- src/models/jwt/jwt_headers.rs
+++ src/models/jwt/jwt_headers.rs
@@ -5,8 +5,7 @@ use crate::models::jwt::jwt_token_type::JwtTokenType;
 /// The header of a JWT token. Used to identify what signing algorithm is used and what type of
 /// token it is.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct JwtHeader<T = JwtTokenType>
-{
+pub struct JwtHeader<T = JwtTokenType> {
     /// The algorithm of that the server used to sign the JWT token. Possible values can be found in
     /// [RFC 7518](https://www.rfc-editor.org/rfc/rfc7518#section-3).
     pub alg: String,
@@ -21,7 +20,7 @@ pub struct JwtHeader<T = JwtTokenType>
 }
 
 impl<T> Default for JwtHeader<T> {
-    fn default() -> Self {
+    fn default() -> JwtHeader<T> {
         Self {
             alg: "RS256".to_string(),
             typ: "JWT".to_string(),
